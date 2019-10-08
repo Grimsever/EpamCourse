8f8c928a2d879fd3106713830608e50fb79ae6e1
Grimsever <fupkaa@gmail.com>
2019-10-08 19:51:40 +0300

    Add task of module 1

---
+++ Module1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value number a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value number b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = b + a;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"Value a={a}");
+            Console.WriteLine($"Value b={b}");
+        }
+    }
+}
