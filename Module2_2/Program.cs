9b4dba92dd9089e66f6919ad2caebafa48ca7d85
Grimsever <fupkaa@gmail.com>
2019-10-14 19:31:16 +0300

    Add task 2 of module 2

---
+++ Module2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the integer number");
+            int N = int.Parse(Console.ReadLine());
+            if (N >= 18 && N % 2 == 0)
+            {
+                Console.WriteLine("Сongratulations on coming of age");
+            }
+            if (N < 18 && N % 2 != 0 && N >= 13)
+            {
+                Console.WriteLine("Congratulation on starting  high school");
+            }
+        }
+    }
+}
