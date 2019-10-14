1499577b79e7e5411085ae7808a2836ae9c4f357
Grimsever <fupkaa@gmail.com>
2019-10-14 19:33:56 +0300

    Add task 3 of module 2

---
+++ Module2_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Module2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            string a = Console.ReadLine();
+            Console.WriteLine("Enter the second number");
+            string b= Console.ReadLine();
+            if (double.TryParse(a, out double num_1) && double.TryParse(b,out double num_2))
+            {
+                num_1 = num_1 + num_2;
+                num_2 = num_1 - num_2;
+                num_1 = num_1 - num_2;
+                Console.WriteLine($"Number 1: {num_1}");
+                Console.WriteLine($"Number 2: {num_2}");
+            }
+            else
+            {
+                Console.WriteLine("You made a mistake while entering numbers");
+            }
+        }
+    }
+}
