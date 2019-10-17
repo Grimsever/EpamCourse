f82b9df11ff4ed53f59685805609c18aa421df6e
Vladislav Furmanov <39351437+Grimsever@users.noreply.github.com>
2019-10-17 19:21:16 +0300

    fix name of variablse

--- Module2_2/Program.cs
+++ Module2_2/Program.cs
@@ -7,12 +7,12 @@ namespace Module2_2
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the integer number");
-            int N = int.Parse(Console.ReadLine());
-            if (N >= 18 && N % 2 == 0)
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 18 && n % 2 == 0)
             {
                 Console.WriteLine("Сongratulations on coming of age");
             }
-            if (N < 18 && N % 2 != 0 && N >= 13)
+            if (n < 18 && n % 2 != 0 && n >= 13)
             {
                 Console.WriteLine("Congratulation on starting  high school");
             }
