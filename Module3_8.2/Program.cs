86090e0adc58276bccf232a4041067341f9a682c
Grimsever <fupkaa@gmail.com>
2019-11-01 20:04:52 +0300

    Fixes validation

--- Module3_8.2/Program.cs
+++ Module3_8.2/Program.cs
@@ -8,31 +8,36 @@ namespace Module3_8._2
         {
             Console.WriteLine($"You have a function: f(x)=2x-3");
             Console.WriteLine("Enter the accuracy");
-            double.TryParse(Console.ReadLine(), out double accuracy);
+            bool isCorrect = double.TryParse(Console.ReadLine(), out double accuracy);
             Console.WriteLine("Enter the start and the end of segment");
-            double.TryParse(Console.ReadLine(),out double start);
-            double.TryParse(Console.ReadLine(), out double end);
-            if (GetFunction(start) * GetFunction(end) < 0)
+            if (isCorrect && double.TryParse(Console.ReadLine(), out double start) && double.TryParse(Console.ReadLine(), out double end))
             {
-                double xI = 0;
-                while (Math.Abs(end - start) > accuracy)
+                if (GetFunction(start) * GetFunction(end) < 0)
                 {
-                    double dx = (end - start) / 2.0;
-                    xI = start + dx;
-                    if (Math.Sign(GetFunction(start)) != Math.Sign(GetFunction(xI)))
+                    double xI = 0;
+                    while (Math.Abs(end - start) > accuracy)
                     {
-                        end = xI;
+                        double dx = (end - start) / 2.0;
+                        xI = start + dx;
+                        if (Math.Sign(GetFunction(start)) != Math.Sign(GetFunction(xI)))
+                        {
+                            end = xI;
+                        }
+                        else
+                        {
+                            start = xI;
+                        }
                     }
-                    else
-                    {
-                        start = xI;
-                    }
+                    Console.WriteLine($"Solution the function is x= {xI}");
                 }
-                Console.WriteLine($"Solution the function is x= {xI}");
+                else
+                {
+                    Console.WriteLine("Segment that you inputting is not correct");
+                }
             }
             else
             {
-                Console.WriteLine("Segment that you inputting is not correct");
+                Console.WriteLine("You made mistake in inputting!!");
             }
         }
         private static double GetFunction(double x)
